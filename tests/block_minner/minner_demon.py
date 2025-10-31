3d81c39335640e417e1a08384098b696f1a2eb4b
cat80 <seanpaninsapce@gmail.com>
2025-10-31 12:03:37 +0800

    异步挖矿示例代码

--- tests/block_minner/minner_demon.py
+++ tests/block_minner/minner_demon.py
@@ -23,6 +23,7 @@ class BlockChain:
             self.blocks.append([get_hash('778899'), 0])
     def add_block(self,block):
         self.blocks.append(block)
+        return True
     def get_best_tip(self):
         if self.blocks:
             return self.blocks[-1]
@@ -31,27 +32,49 @@ class BlockChain:
         self.blocks = []
         self.init_block()
 
+def do_minner_blocking_loop(prev_hash:bytes,stop_event:threading.Event):
+    """
+        把挖矿需要顾不得计算的独立出来
+    """
+    nonce = 0
+    while True:
+        if nonce % 10000 == 0:
+            if stop_event.is_set():
+                log.debug(f'recv quit event signal,quit mining...')
+                return None
+        n_bytes = nonce.to_bytes(4, byteorder="little")
+        new_hash = get_hash(prev_hash + n_bytes)
+        if new_hash.startswith(b'\x00\x00\x00'):
+            log.debug(f'[mining thread]find block nonce:{nonce},hash:{new_hash.hex()},prev:{prev_hash.hex()}')
+            #直接返回交给eventloop处理
+            return [new_hash, nonce]
+        nonce += 1
+
 class Minner:
     def __init__(self,block_chain:BlockChain):
         """
             写一个简单的挖矿程序的实现
         """
-        self.event = threading.Event()
+        self.stop_mining_event = threading.Event()
         self.chian = block_chain
-        self.minner_thread = None
         self.queue = queue.Queue()
         self.check_queue_thread =None
+        self.minner_task =None
+        self.input_task = None
+        # self.minner_task =None
         pass
 
-    def start(self):
+    async def start(self):
         """
-            开始线程
+            开始任务
         :return:
         """
-        self.check_queue_thread = threading.Thread(target=self.check_queue)
-        self.restart_minner()
-        self.check_queue_thread.start()
-        self.handler_input()
+
+        # self.check_queue_thread = threading.Thread(target=self.check_queue)
+        # self.check_queue_thread.start()
+        self.input_task =  asyncio.create_task(self.handler_input())
+        await  self.restart_minner()
+        await self.input_task
     def check_queue(self):
         """
             查询队列状态
@@ -69,52 +92,52 @@ class Minner:
             except Exception as e:
                 log.exception('exception occur')
                 time.sleep(5)
-    def handler_input(self):
+
+    async  def handler_input(self):
         while True:
-            cmd = input('>')
+            # 异步等等
+            cmd = await asyncio.to_thread(  input,'>')
             if cmd ==  'restart':
                 log.debug('restart ming while empty chian blocks...')
                 self.chian.reset_chian()
-                self.restart_minner()
+                await self.restart_minner()
             elif cmd == 'add':
                 # 增加一个假的 fake
                 current_time =  int(time.time())
                 fake_hash = get_hash(current_time)
                 self.chian.blocks.append([fake_hash,current_time])
                 log.debug(f'add fake block.restart mining...')
-                self.restart_minner()
+                await self.restart_minner()
             else:
                 log.debug(f'invalid cmd:{cmd}')
 
 
-    def restart_minner(self):
+    async def restart_minner(self):
         # 这里需要重新启动挖矿线程，让之前的消息退出
-        self.event.set()
-        while self.minner_thread and  self.minner_thread.is_alive():
-            time.sleep(1)
-            log.debug(f'waiting mining thread quit....')
-        self.event.clear()  # 清除消息量
-        self.minner_thread = threading.Thread(target=self.do_minner)
-        self.minner_thread.start()
+        self.stop_mining_event.set()
+        while self.minner_task and not self.minner_task.done():
+            log.debug(f'waiting mining task quit....')
+            log.debug(f'waiting mining task quit....')
 
-    def do_minner(self):
+        self.stop_mining_event.clear()  # 清除消息量
+        self.minner_task = asyncio.create_task(self.do_minner())
+
+    async def do_minner(self):
         # 新区块挖矿
         log.debug('start mining....')
-        nonce = 0
-        prev_hash = self.chian.get_best_tip()[0]
         while True:
-            if nonce % 10000 ==0:
-                if self.event.is_set():
-                    log.debug(f'recv quit event signal,quit mining...')
-                    break
-            n_bytes = nonce.to_bytes(4,byteorder="little")
-            new_hash = get_hash(prev_hash+n_bytes)
-            if new_hash.startswith(b'\x00\x00\x00'):
-                log.debug(f'find block nonce:{nonce},hash:{new_hash.hex()},prev:{prev_hash.hex()}')
-                self.queue.put([new_hash,nonce])
-                prev_hash = new_hash
-                nonce = -1
-            nonce +=1
+            prev_hash = self.chian.get_best_tip()[0]
+            new_block = await asyncio.to_thread(do_minner_blocking_loop,prev_hash,self.stop_mining_event)
+            if new_block:
+                log.debug(f'find new block,block hash {new_block[0].hex()},nonce:{new_block[1]}')
+                if self.chian.add_block(block=new_block):
+                    log.debug('new block add success')
+                else:
+                    log.debug('new block add fail.')
+                log.debug(f'chian len:{len(self.chian.blocks)}')
+            else:
+                # 直接结束
+                break
         log.debug('mining quit....')
 def check():
     prev_hash = bytes.fromhex('000044a79098092823b04575672fd8d9c29e6d44d6d61daf4edeb6d5ba6eab25')
@@ -137,12 +160,11 @@ def run_main1():
     for block  in chian.blocks:
         print(block[0].hex(),block[1])
     # print(chian.mainChian)
-def run_main():
+async def run_main():
     log.debug('开始执行')
     chian = BlockChain()
     chian.init_block()
     minner = Minner(chian)
-    minner.start()
-    log.debug('执行完成')
+    await minner.start()
 if __name__ == "__main__":
-    run_main()
+    asyncio.run(run_main())
